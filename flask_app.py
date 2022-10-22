3ed8f4286ad170d8d1e9df76566309f446d28057
Avery2 <justinaverychan@gmail.com>
2022-10-22 01:16:04 -0700

    fix: try to use global flask context

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, g
 
 app = Flask(__name__)
 
 
 output_anchor = '<!-- output anchor -->'
 sum_anchor = '<!-- sum anchor -->'
-acc_sum = 0
+g.acc_sum = 0
 
 
 @app.route('/')
@@ -18,7 +18,7 @@ def my_form_post():
     text = get_flask_input()
 
     if (text.isnumeric()):
-        acc_sum += int(text)
+        g.acc_sum += int(text)
 
     return flask_print(text)
 
@@ -28,4 +28,4 @@ def get_flask_input():
 
 
 def flask_print(text):
-    return render_template('my-form.html').replace(output_anchor, text).replace(sum_anchor, f'sum: {acc_sum}')
+    return render_template('my-form.html').replace(output_anchor, text).replace(sum_anchor, f'sum: {g.acc_sum}')
